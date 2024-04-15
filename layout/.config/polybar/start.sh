4cd547ad4337a8cfa7e6c42c04fae86fb51027b4
widndowchips <johndoe@example.com>
2024-04-15 12:51:10 +0500

    added tray functionality properly

--- layout/.config/polybar/start.sh
+++ layout/.config/polybar/start.sh
@@ -2,7 +2,7 @@
 
 ps -ef | grep hideIt | grep -v grep | awk '{print $2}' | xargs kill
 killall -q polybar
-sleep 0.3
-polybar & 
-sleep 0.3
-bash ~/.config/polybar/hideIt.sh -w --name '^polybar-i3barclone_eDP-1$' -p 2 -d top -r 0x0+1920+2 --interval 0.016 -s 9
+polybar i3barclone & polybar systray &
+sleep 1
+bash ~/.config/polybar/hideIt.sh -w --name '^polybar-i3barclone_eDP-1$' -p 2 -d top -r 0x0+1920+2 --interval 0.01
+polybar-msg cmd hide
