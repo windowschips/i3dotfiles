7f3ea550812d38c08b8b4b3e4bd71db4f2cb9b9e
windowschips <none@example.com>
2024-10-14 17:10:38 +0500

    Revert rounding its ugly LOL

--- screenshot.sh
+++ screenshot.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 # Run the command and capture its output
-output=$(eval "maim -s --highlight --color=.3,.3,.3,0.9 | ~/.cargo/bin/shadower -r 8 --padding-x 20 --padding-y 20 | xclip -selection clipboard -t image/png" 2>&1)
+output=$(eval "maim -s --highlight --color=.3,.3,.3,0.9 | ~/.cargo/bin/shadower -r 0 --padding-x 20 --padding-y 20 | xclip -selection clipboard -t image/png" 2>&1)
 
 # Check if the output contains alphabetic characters
 if [[ $output =~ [a-zA-Z] ]]; then
