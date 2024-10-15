636e6673728ee132a0594580293245791b6c6021
windowschips <none@example.com>
2024-10-15 20:12:21 +0500

    Screenshot tool finally fixed

--- apps.sh
+++ apps.sh
@@ -5,7 +5,8 @@ function main {
     	wifi-menu) kitty nmtui;;
     	bluetooth-menu) kitty bluetui;;
     	screenlock) xlock -font -*-helvetica-*-*-normal-*-25-*-*-*-*-*-*-* -mode space +description +showdate;;
-    	screenshot) ~/.dotfiles/screenshot.sh;;
+    	ssselection) maim -s --highlight --color=.3,.3,.3,0.9 | xclip -selection clipboard -t image/png; xclip -selection clipboard -t image/png -o > ~/Pictures/Screenshots/$(date +%F-%H:%M:%S).png && dunstify -a "Screenshot Tool" "Selection screenshot successful" "The image has been copied to clipboard and saved as a file.";;
+       	ssfull) maim | xclip -selection clipboard -t image/png; xclip -selection clipboard -t image/png -o > ~/Pictures/Screenshots/$(date +%F-%H:%M:%S).png && dunstify -a "Screenshot Tool" "Fullscreen screenshot successful" "The image has been copied to clipboard and saved as a file.";;
         calendar) gnome-calendar;;
         hardware-monitor) kitty btop -p 0;;
         
