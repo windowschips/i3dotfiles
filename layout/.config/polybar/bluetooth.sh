ab6a71cd3a6f084adbb62cf2e455b0a3b9749b8d
windowschips <none@example.com>
2024-10-26 17:58:53 +0500

    fix bluetooth

--- layout/.config/polybar/bluetooth.sh
+++ layout/.config/polybar/bluetooth.sh
@@ -3,9 +3,11 @@
 if [ $(bluetoothctl show | grep "Powered: yes" | wc -c) -eq 0 ]; then
     echo "%{F#66ffffff}󰂲" # disabled
 else
-    echo "%{F#5577ff}󰂯" # enabled
+    
 
-    if [ $(echo info | bluetoothctl | grep 'Device' | wc -c) -eq 0 ]; then 
+    if [ $(echo info | bluetoothctl | grep 'Device' | wc -c) -eq 31 ]; then 
         echo "%{F#ffffff}󰂯" # enabled alt
+    else
+        echo "%{F#5577ff}󰂯" # enabled
     fi
 fi
