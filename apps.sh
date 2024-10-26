839f586e14445e031b5224f4c24ef0d538b83256
windowschips <none@example.com>
2024-10-26 18:06:55 +0500

    change apps

--- apps.sh
+++ apps.sh
@@ -3,11 +3,10 @@
 function main {
     case $1 in
     	wifi-menu) kitty nmtui;;
-    	bluetooth-menu) kitty bluetui;;
+    	bluetooth-menu) blueman-manager;;
     	screenlock) slock;;
     	ssselection) maim -s --color=.3,.3,.3,0.9 | ~/.cargo/bin/shadower -r 0 --padding-x 20 --padding-y 20 | xclip -selection clipboard -t image/png; xclip -selection clipboard -t image/png -o > ~/Pictures/Screenshots/$(date +%F-%H:%M:%S).png && dunstify -a "Screenshot Tool" "Selection screenshot successful" "The image has been copied to clipboard and saved as a file.";;
        	ssfull) maim | ~/.cargo/bin/shadower -r 0 --padding-x 20 --padding-y 20 | xclip -selection clipboard -t image/png; xclip -selection clipboard -t image/png -o > ~/Pictures/Screenshots/$(date +%F-%H:%M:%S).png && dunstify -a "Screenshot Tool" "Fullscreen screenshot successful" "The image has been copied to clipboard and saved as a file.";;
-        calendar) gnome-calendar;;
         hardware-monitor) kitty btop -p 0;;
         
         *)
