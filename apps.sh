e108c8e6673190656b5f8f53c8798a993558f80c
windowschips <none@example.com>
2024-10-26 17:24:46 +0500

    maim

--- apps.sh
+++ apps.sh
@@ -5,7 +5,7 @@ function main {
     	wifi-menu) kitty nmtui;;
     	bluetooth-menu) kitty bluetui;;
     	screenlock) slock;;
-    	ssselection) maim -s --highlight --color=.3,.3,.3,0.9 | ~/.cargo/bin/shadower -r 0 --padding-x 20 --padding-y 20 | xclip -selection clipboard -t image/png; xclip -selection clipboard -t image/png -o > ~/Pictures/Screenshots/$(date +%F-%H:%M:%S).png && dunstify -a "Screenshot Tool" "Selection screenshot successful" "The image has been copied to clipboard and saved as a file.";;
+    	ssselection) maim -s --color=.3,.3,.3,0.9 | ~/.cargo/bin/shadower -r 0 --padding-x 20 --padding-y 20 | xclip -selection clipboard -t image/png; xclip -selection clipboard -t image/png -o > ~/Pictures/Screenshots/$(date +%F-%H:%M:%S).png && dunstify -a "Screenshot Tool" "Selection screenshot successful" "The image has been copied to clipboard and saved as a file.";;
        	ssfull) maim | ~/.cargo/bin/shadower -r 0 --padding-x 20 --padding-y 20 | xclip -selection clipboard -t image/png; xclip -selection clipboard -t image/png -o > ~/Pictures/Screenshots/$(date +%F-%H:%M:%S).png && dunstify -a "Screenshot Tool" "Fullscreen screenshot successful" "The image has been copied to clipboard and saved as a file.";;
         calendar) gnome-calendar;;
         hardware-monitor) kitty btop -p 0;;
