51c7953178bb1d2abfe2c4d1ddce416ec0471480
windowschips <none@example.com>
2024-10-28 18:42:37 +0500

    add delayed screenshot tool

--- apps.sh
+++ apps.sh
@@ -7,9 +7,9 @@ function main {
     	screenlock) slock;;
     	ssselection) maim -s --color=.3,.3,.3,0.9 | ~/.cargo/bin/shadower -r 0 --padding-x 20 --padding-y 20 | xclip -selection clipboard -t image/png; xclip -selection clipboard -t image/png -o > ~/Pictures/Screenshots/$(date +%F-%H:%M:%S).png && dunstify -a "Screenshot Tool" "Selection screenshot successful" "The image has been copied to clipboard and saved as a file.";;
        	ssfull) maim | ~/.cargo/bin/shadower -r 0 --padding-x 20 --padding-y 20 | xclip -selection clipboard -t image/png; xclip -selection clipboard -t image/png -o > ~/Pictures/Screenshots/$(date +%F-%H:%M:%S).png && dunstify -a "Screenshot Tool" "Fullscreen screenshot successful" "The image has been copied to clipboard and saved as a file.";;
-        
+        ssdelay) dunstify -a "Screenshot Tool" "Delayed fullscreen screenshot" "Attempting screenshot in 3 seconds..." -t 2500 && sleep 3 && maim | ~/.cargo/bin/shadower -r 0 --padding-x 20 --padding-y 20 | xclip -selection clipboard -t image/png; xclip -selection clipboard -t image/png -o > ~/Pictures/Screenshots/$(date +%F-%H:%M:%S).png && dunstify -a "Screenshot Tool" "Delayed fullscreen screenshot successful" "The image has been copied to clipboard and saved as a file.";;
         *)
-            echo "Usage: $0 {wifi-menu|bluetooth-menu|screenlock|screenshot|calendar|hardware-monitor}"
+            echo "Invalid argument, check script for list of apps you can invoke"
             exit 1
             ;;
     esac
