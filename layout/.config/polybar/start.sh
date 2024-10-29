e6d3d67c8378159b8c58f45a72b1e24ff270e2d9
windowschips <none@example.com>
2024-10-29 11:09:12 +0500

    hide anims

--- layout/.config/polybar/start.sh
+++ layout/.config/polybar/start.sh
@@ -4,4 +4,4 @@ ps -ef | grep hideIt | grep -v grep | awk '{print $2}' | xargs kill
 killall -q polybar
 polybar i3barclone &
 sleep 1
-bash ~/.config/polybar/hideIt.sh -w --name '^polybar-i3barclone_eDP-1$' -p 2 -d top -r 0x0+1920+2 --interval 0.01 -s 7
+bash ~/.config/polybar/hideIt.sh -w --name '^polybar-i3barclone_eDP-1$' -p 2 -d top -r 0x0+1920+2 --interval 0.005 -s 21
